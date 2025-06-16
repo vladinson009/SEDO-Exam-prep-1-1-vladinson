edd9fdab8175173a540d7668febfbc88db673999
SUContent <134996680+SUContent@users.noreply.github.com>
2025-06-16 14:47:10 +0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+document.getElementById('btn-add').addEventListener('click', () => {
+  const a = Number(document.getElementById('a').value);
+  const b = Number(document.getElementById('b').value);
+  document.getElementById('result').textContent = add(a, b);
+});
+
+document.getElementById('btn-subtract').addEventListener('click', () => {
+  const a = Number(document.getElementById('a').value);
+  const b = Number(document.getElementById('b').value);
+  document.getElementById('result').textContent = subtract(a, b);
+});
