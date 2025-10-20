769ad46db518c76dbfa05ca1b8fb855b76e1d3aa
Vladimir Gulev <83925378+vladinson009@users.noreply.github.com>
2025-10-20 20:57:03 +0200

    SQUASH 3 IN 1: Refactor handlers (#5)
    SQUASH and MERGE

--- script.js
+++ script.js
@@ -15,19 +15,23 @@ function multiply(a, b) {
 }
 
 document.getElementById('btn-add').addEventListener('click', () => {
-  const a = Number(document.getElementById('a').value);
-  const b = Number(document.getElementById('b').value);
-  document.getElementById('result').textContent = add(a, b);
+  const first = Number(document.getElementById('a').value);
+  const second = Number(document.getElementById('b').value);
+  document.getElementById('result').textContent = add(first, second);
 });
 
 document.getElementById('btn-subtract').addEventListener('click', () => {
+
+  const first = Number(document.getElementById('a').value);
+  const second = Number(document.getElementById('b').value);
+  document.getElementById('result').textContent = subtract(first, second);
+});
+//TODO: Tasks todo
   // Comment Here 3!
-  const a = Number(document.getElementById('a').value);
-  const b = Number(document.getElementById('b').value);
-  document.getElementById('result').textContent = subtract(a, b);
 });
 document.getElementById('btn-multiply').addEventListener('click', () => {
   const a = Number(document.getElementById('a').value);
   const b = Number(document.getElementById('b').value);
   document.getElementById('result').textContent = multiply(a, b);
 });
+
