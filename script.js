c56b50b680a4b77506e02523a74d8982eaade3b9
Vladimir Gulev <vladinson009@icloud.com>
2025-10-20 20:09:55 +0200

    SQUASH 3 IN 1: Refactor handlers

--- script.js
+++ script.js
@@ -7,13 +7,14 @@ function subtract(a, b) {
 }
 
 document.getElementById('btn-add').addEventListener('click', () => {
-  const a = Number(document.getElementById('a').value);
-  const b = Number(document.getElementById('b').value);
-  document.getElementById('result').textContent = add(a, b);
+  const first = Number(document.getElementById('a').value);
+  const second = Number(document.getElementById('b').value);
+  document.getElementById('result').textContent = add(first, second);
 });
 
 document.getElementById('btn-subtract').addEventListener('click', () => {
-  const a = Number(document.getElementById('a').value);
-  const b = Number(document.getElementById('b').value);
-  document.getElementById('result').textContent = subtract(a, b);
+  const first = Number(document.getElementById('a').value);
+  const second = Number(document.getElementById('b').value);
+  document.getElementById('result').textContent = subtract(first, second);
 });
+//TODO: Tasks todo
