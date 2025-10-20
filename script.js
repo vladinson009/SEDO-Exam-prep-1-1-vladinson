a3d4e4ff41a090fe0fe08740c5cac147e5f01aa5
Vladimir Gulev <83925378+vladinson009@users.noreply.github.com>
2025-10-20 20:55:03 +0200

    Clean Message after SQUASH HERE (#6)
    SQUASH and MERGE

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ function subtract(a, b) {
   }
   return a - b;
 }
+function multiply(a, b) {
+  return a * b;
+}
 
 document.getElementById('btn-add').addEventListener('click', () => {
   const a = Number(document.getElementById('a').value);
@@ -22,4 +25,9 @@ document.getElementById('btn-subtract').addEventListener('click', () => {
   const a = Number(document.getElementById('a').value);
   const b = Number(document.getElementById('b').value);
   document.getElementById('result').textContent = subtract(a, b);
+});
+document.getElementById('btn-multiply').addEventListener('click', () => {
+  const a = Number(document.getElementById('a').value);
+  const b = Number(document.getElementById('b').value);
+  document.getElementById('result').textContent = multiply(a, b);
 });
